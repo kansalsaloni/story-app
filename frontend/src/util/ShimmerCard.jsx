dd0a986ba69c88e36434f4e4e84424e34cb0766f
salonikansal <salonikansal9557@gmail.com>
2024-09-23 20:18:59 +0530

    shimmer card

---
+++ frontend/src/util/ShimmerCard.jsx
@@ -0,0 +1,11 @@
+import '../Style/ShimmerCardStyle.css';
+
+const ShimmerCard = () => (
+    <div className="shimmer-card">
+      <div className="shimmer-card-title-container" >
+        <div className="shimmer-card-title" />
+        <div className="shimmer-card-text" />
+      </div>
+    </div>
+  );
+  export default ShimmerCard;
